fafb4918859b554801058f919619c3049ba6c7d6
sh1kasi <elfarrel07@gmail.com>
2026-07-19 06:47:41 +0700

    feat(1c): component state, event handlers

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,54 +1,33 @@
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
-}
+import { useState } from "react"
 
-const Part = (props) => {
-  return (
-    <p>{props.part} {props.exercise}</p>
-  )
-}
+const Display = ({ counter }) => <div>{counter}</div>
 
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.part1} exercise={props.exercises1} />
-      <Part part={props.part2} exercise={props.exercises2} />
-      <Part part={props.part3} exercise={props.exercises3} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <p>Number of exercises {props.total}</p>
-  )
-}
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
-const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const [counter, setCounter] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+  console.log('rendering...', counter)
+
   return (
     <div>
-      <Header course={course.name} />
-      <Content part1={course.parts[0].name} exercises1={course.parts[0].exercises} part2={course.parts[1].name} exercises2={course.parts[1].exercises} part3={course.parts[2].name} exercises3={course.parts[2].exercises}/>
-      <Total total={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
+      <Display counter={counter}/>
+      <Button 
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button 
+        onClick={setToZero}
+        text='zero'
+      />
+      <Button 
+        onClick={decreaseByOne}
+        text='minus'
+      />
     </div>
   )
 }
