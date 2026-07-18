01a148cc08ba7cb42dac65bc069784c89a3f30c8
neutralcheeze <elfarrel07@gmail.com>
2026-07-18 11:47:23 +0700

    feat(1.2): refactor Content components for each Part

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,9 +4,25 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
-    <p>{props.part} {props.exercise}</p>   
+    <p>{props.part} {props.exercise}</p>
+  )
+}
+
+const Content = () => {
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  return (
+    <div>
+      <Part part={part1} exercise={exercises1} />
+      <Part part={part2} exercise={exercises2} />
+      <Part part={part3} exercise={exercises3} />
+    </div>
   )
 }
 
@@ -18,19 +34,14 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
   const exercises1 = 10
-  const part2 = 'Using props to pass data'
   const exercises2 = 7
-  const part3 = 'State of a component'
   const exercises3 = 14
 
   return (
     <div>
-      <Header course={course}/>
-      <Content part={part1} exercise={exercises1}/> 
-      <Content part={part2} exercise={exercises2}/> 
-      <Content part={part3} exercise={exercises3}/> 
+      <Header course={course} />
+      <Content />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
