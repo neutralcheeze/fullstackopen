974af08807bff550033666bec0f078f4284dc085
sh1kasi <elfarrel07@gmail.com>
2026-08-13 22:19:26 +0700

    feat(exercise 1.1 - 1.5): late commit for course info

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,56 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercise}</p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} exercise={props.exercises1} />
+      <Part part={props.part2} exercise={props.exercises2} />
+      <Part part={props.part3} exercise={props.exercises3} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.total}</p>
+  )
+}
+
+const App = () => {
+const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content part1={course.parts[0].name} exercises1={course.parts[0].exercises} part2={course.parts[1].name} exercises2={course.parts[1].exercises} part3={course.parts[2].name} exercises3={course.parts[2].exercises}/>
+      <Total total={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
+    </div>
+  )
+}
+
+export default App
---
+++ part2/unicafe/src/App.jsx
@@ -0,0 +1,95 @@
+import { useState } from 'react'
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>{props.text}</button>
+  )
+}
+
+const StatisticLine = (props) => {
+  return (
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+  )
+}
+
+const Statistics = (props) => {
+
+  const totalFeedback = 0 + props.feedback.good + props.feedback.neutral + props.feedback.bad
+  const average = (props.feedback.good * 1 + props.feedback.neutral * 0 + props.feedback.bad * -1) / totalFeedback
+  const positivePercentage = (props.feedback.good / totalFeedback) * 100
+
+  return (
+    <div>
+      <table>
+        <StatisticLine text="good" value={props.feedback.good} />
+        <StatisticLine text="neutral" value={props.feedback.neutral} />
+        <StatisticLine text="bad" value={props.feedback.bad} />
+        <tr>
+          <td>all</td>
+          <td>{props.feedback.good + props.feedback.neutral + props.feedback.bad}</td>
+        </tr>
+        <tr>
+          <td>average</td>
+          <td>{average}</td>
+        </tr>
+        <tr>
+          <td>percentage</td>
+          <td>{positivePercentage} %</td>
+        </tr>
+      </table>
+    </div>
+  )
+}
+
+const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  })
+
+  const [total, setTotal] = useState(0)
+
+  const handleFeedbacks = (currentFeedback) => {
+    let good = feedback.good
+    let neutral = feedback.neutral
+    let bad = feedback.bad
+
+    if (currentFeedback == "good") {
+      good = good + 1
+    } else if (currentFeedback == "neutral") {
+      neutral = neutral + 1
+    } else if (currentFeedback == "bad") {
+      bad = bad + 1
+    }
+
+    const newFeedback = {
+      good: good,
+      neutral: neutral,
+      bad: bad
+    }
+
+    setFeedback(newFeedback)
+    setTotal(total + 1)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <div>
+        <Button onClick={() => handleFeedbacks("good")} text="good" />
+        <Button onClick={() => handleFeedbacks("neutral")} text="neutral" />
+        <Button onClick={() => handleFeedbacks("bad")} text="bad" />
+      </div>
+      <h1>
+        statistics
+      </h1>
+      {total > 0 ? <Statistics feedback={feedback} /> : <div>no feedback given</div>}
+    </div>
+  )
+}
+
+export default App
